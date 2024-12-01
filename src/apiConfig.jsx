237607fb007a631112c2339a5428da3d106daac8
zahratalaei <52117585+zahratalaei@users.noreply.github.com>
2024-12-01 22:48:03 +0000

    updated .gitignore and apiConfig file

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -1,7 +1,7 @@
 const apiConf = {
      baseUrl : "https://api.themoviedb.org/3/",
-     apiKey: "?api_key=2af988655577d05985545caa7786f118",
      searchUrl : "https://api.themoviedb.org/3/search/movie",
+     apiKey: "?api_key=2af988655577d05985545caa7786f118",
      originalImage:(imgPath)=>`https://image.tmdb.org/t/p/original/${imgPath}`,
      w500Image:(imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`,
      logoUrl: `https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg`
